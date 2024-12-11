490850f6affd0f7b75405c0177fa74fc8bb35e5d
Elderes <abraao.borges@academico.ifpb.edu.br>
2024-12-11 16:49:08 -0300

    first commit. initial building state

---
+++ src/main/java/com/ifpb/edu/so/Process/Reader.java
@@ -0,0 +1,7 @@
+package com.ifpb.edu.so.Process;
+
+public class Reader extends Process {
+    public Reader(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/com/ifpb/edu/so/Process/Writer.java
@@ -0,0 +1,7 @@
+package com.ifpb.edu.so.Process;
+
+public class Writer extends Process {
+    public Writer(String name) {
+        super(name);
+    }
+}
